375787c237949325249e6d59486cb838ce1e08d4
Vladimir <v.petrzhikovskiy@dexpa.io>
2021-10-16 18:19:51 +0300

    Rework profiler

--- ton-indexer-profile/src/lib.rs
+++ ton-indexer-profile/src/lib.rs
@@ -1,57 +1,156 @@
-#[cfg(feature = "active")]
-#[macro_export]
-macro_rules! profile_start {
-    ($name:ident) => {
-        let $name = std::time::Instant::now();
-    };
+use std::io::Write;
+use std::path::Path;
+
+use bincode::Options;
+use byteorder::{LittleEndian, WriteBytesExt};
+use crossbeam::channel::Sender;
+use serde::{Deserialize, Serialize};
+
+pub static mut COLLECTOR: Collector = Collector { tx: None };
+
+pub fn init<P: AsRef<Path>>(logs: P) -> std::io::Result<()> {
+    let (tx, rx) = crossbeam::channel::unbounded();
+    let out_file = std::fs::OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(logs.as_ref())?;
+
+    std::thread::spawn(|| {
+        let mut writer = std::io::BufWriter::new(out_file);
+        let options = bincode::DefaultOptions::new().with_varint_encoding();
+        let mut buffer = Vec::with_capacity(1024);
+        for record in rx {
+            buffer.clear();
+            options.serialize_into(&mut buffer, &record).unwrap();
+            if let Err(e) = writer.write_u16::<LittleEndian>(buffer.len() as u16) {
+                log::error!("Failed saving profile: {}", e);
+            }
+            if let Err(e) = writer.write_all(&buffer) {
+                log::error!("Failed saving profile: {}", e);
+            }
+        }
+    });
+
+    unsafe {
+        COLLECTOR.tx = Some(tx);
+    }
+
+    Ok(())
+}
+
+#[derive(Default)]
+pub struct Collector {
+    tx: Option<Sender<Record>>,
+}
+
+impl Collector {
+    pub fn add_record(&self, record: Record) {
+        if let Some(tx) = &self.tx {
+            tx.send(record).ok();
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Record {
+    x: Option<u64>,
+    /// NOTE: 500 years should be enough
+    y: u64,
+    id: &'static str,
+    path: &'static str,
+}
+
+impl Record {
+    pub fn new(
+        time: &std::time::Instant,
+        x: Option<u64>,
+        id: &'static str,
+        path: &'static str,
+    ) -> Self {
+        Self {
+            y: time.elapsed().as_nanos() as u64,
+            x,
+            id,
+            path,
+        }
+    }
 }
 
 #[cfg(feature = "active")]
-#[macro_export]
-macro_rules! profile_elapsed {
-    ($name:ident) => {
-        ::log::trace!(
-            "#PERF#{{\"took\":{},\"context\":\"{}\",\"path\":\"{{{}::{}::{}}}\"}}#PERF#",
-            $name.elapsed().as_nanos(),
-            stringify!($name),
-            file!(),
-            module_path!(),
-            line!()
-        )
-    };
-    ($name:ident, $ctx:literal) => {
-        // log_record!(
-        //     &$name,
-        //     $ctx,
-        //     &format!("{}::{}::{}", file!(), module_path!(), line!()),
-        // );
-        ::log::trace!(
-            "#PERF#{{\"took\":{},\"context\":\"{}\",\"path\":\"{{{}::{}::{}}}\"}}#PERF#",
-            $name.elapsed().as_nanos(),
-            $ctx,
-            file!(),
-            module_path!(),
-            line!()
-        )
-    };
+mod macros {
+    #[macro_export]
+    macro_rules! span {
+        ($name:tt, $expr:expr) => {{
+            let __time = ::std::time::Instant::now();
+            let __res = $expr;
+            // SAFETY: yes
+            unsafe {
+                $crate::COLLECTOR.add_record($crate::Record::new(
+                    &__time,
+                    None,
+                    $crate::span!(@expand_id $name),
+                    module_path!(),
+                ));
+            }
+            __res
+        }};
+
+        (@expand_id $id:literal) => { $id };
+        (@expand_id $id:ident) => { stringify!($id) };
+    }
+
+    #[macro_export]
+    macro_rules! start {
+        ($name:ident) => {
+            let $name = ::std::time::Instant::now();
+        };
+    }
+
+    #[macro_export]
+    macro_rules! tick {
+        ($name:ident$( => $id:literal)?$(, x = $x:expr)?) => {
+            // SAFETY: yes
+            unsafe {
+                $crate::COLLECTOR.add_record($crate::Record::new(
+                    &$name,
+                    $crate::tick!(@expand_x $($x)?),
+                    $crate::tick!(@expand_id $name $($id)?),
+                    module_path!(),
+                ));
+            }
+        };
+
+        (@expand_x $x:expr) => { Some(From::from($x)) };
+        (@expand_x) => { None };
+
+        (@expand_id $name:ident $id:literal) => { $id };
+        (@expand_id $name:ident) => { stringify!($id) };
+    }
 }
 
 #[cfg(not(feature = "active"))]
-#[macro_export]
-macro_rules! profile_elapsed {
-    ($name:ident) => {};
-    ($name:ident, $ctx:literal) => {};
-}
+mod macros {
+    #[macro_export]
+    macro_rules! span {
+        ($name:tt, $expr:expr) => {
+            $expr
+        };
+    }
 
-#[cfg(not(feature = "active"))]
-#[macro_export]
-macro_rules! profile_start {
-    ($name:ident) => {};
+    #[macro_export]
+    macro_rules! start {
+        ($name:ident) => {};
+    }
+
+    #[macro_export]
+    macro_rules! tick {
+        ($name:ident$( => $id:literal)?$(, x = $x:expr)?) => {};
+    }
 }
 
 #[macro_export]
 macro_rules! log_record {
-    ($time:expr,$context:expr,$path:expr) => {
+    ($time:expr, $context:expr, $path:expr) => {
         ::log::trace!(
             "#PERF#{{\"took\":{},\"context\":\"{}\",\"path\":\"{}\"}}#PERF#",
             $time.elapsed().as_nanos(),
@@ -79,10 +178,12 @@ mod test {
             .try_init()
             .ok();
         log::info!("Start");
-        profile_start!(lol);
+        start!(lol);
+        start!(lel);
         std::thread::sleep(Duration::from_secs(1));
-        profile_elapsed!(lol, "heh");
+        tick!(lol, "heh");
         std::thread::sleep(Duration::from_millis(100));
-        profile_elapsed!(lol);
+        tick!(lol);
+        tick!(lel);
     }
 }
