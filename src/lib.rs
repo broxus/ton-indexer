a24e38cd2f1825ea5eeda58cf33885a53c17cdd3
Ivan Kalinin <i.kalinin@dexpa.io>
2021-10-31 21:37:36 +0300

    Fix exports

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
 pub use crate::config::*;
 pub use crate::engine::{Engine, EngineStatus, Subscriber};
+pub use crate::storage::BriefBlockMeta;
+
+pub use ton_indexer_alloc as alloc;
+pub use ton_indexer_profile as profile;
+
 mod config;
 mod engine;
 mod network;
 mod storage;
 pub mod utils;
-
-pub use ton_indexer_alloc as alloc;
-pub use ton_indexer_profile as profile;
