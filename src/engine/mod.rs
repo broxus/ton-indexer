b9ac692143e834de23b2dd6feecb1bfa72e0020a
Ivan Kalinin <i.kalinin@dexpa.io>
2021-12-09 21:35:22 +0300

    Refactor engine metrics

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -1112,7 +1112,7 @@ pub struct EngineMetrics {
     pub shard_client_time_diff: AtomicI64,
 }
 
-impl std::fmt::Display for &EngineMetrics {
+impl std::fmt::Display for EngineMetrics {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let last_mc_block_seqno = self.last_mc_block_seqno.load(Ordering::Acquire);
         let last_shard_client_mc_block_seqno = self
@@ -1121,8 +1121,17 @@ impl std::fmt::Display for &EngineMetrics {
         let mc_time_diff = self.mc_time_diff.load(Ordering::Acquire);
         let shard_client_time_diff = self.shard_client_time_diff.load(Ordering::Acquire);
 
-        write!(f, "last_mc_block_seqno {}\nlast_shard_client_mc_block_seqno {}\nmc_time_diff {}\nshard_client_time_diff {}",
-        last_mc_block_seqno,last_shard_client_mc_block_seqno,mc_time_diff,shard_client_time_diff)
+        write!(
+            f,
+            "last_mc_block_seqno {}\n\
+             last_shard_client_mc_block_seqno {}\n\
+             mc_time_diff {}\n\
+             shard_client_time_diff {}",
+            last_mc_block_seqno,
+            last_shard_client_mc_block_seqno,
+            mc_time_diff,
+            shard_client_time_diff
+        )
     }
 }
 
