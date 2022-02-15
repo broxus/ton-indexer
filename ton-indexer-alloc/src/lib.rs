b6e8e927a963a7e74fc3c80c9d1d48efa3bfa7f6
Vladimir <v.petrzhikovskiy@dexpa.io>
2022-02-15 14:04:35 +0300

    Add memory profiling to ton-indexer-alloc

--- ton-indexer-alloc/src/lib.rs
+++ ton-indexer-alloc/src/lib.rs
@@ -1,5 +1,8 @@
 use std::ffi::{c_void, CString};
+use std::os::raw::c_char;
 
+use anyhow::Context;
+use anyhow::Result;
 pub use tikv_jemalloc_ctl::Error;
 use tikv_jemalloc_ctl::{epoch, stats};
 
@@ -61,6 +64,49 @@ pub fn fetch_stats() -> Result<JemallocStats, Error> {
     })
 }
 
+const PROF_ACTIVE: &[u8] = b"prof.active\0";
+const PROF_DUMP: &[u8] = b"prof.dump\0";
+
+pub fn activate_prof() -> Result<()> {
+    log::info!("start profiler");
+    unsafe {
+        if let Err(e) = tikv_jemalloc_ctl::raw::update(PROF_ACTIVE, true) {
+            log::error!("failed to activate profiling: {}", e);
+            anyhow::bail!("failed to activate profiling: {}", e);
+        }
+    }
+    Ok(())
+}
+
+pub fn deactivate_prof() -> Result<()> {
+    log::info!("stop profiler");
+    unsafe {
+        if let Err(e) = tikv_jemalloc_ctl::raw::update(PROF_ACTIVE, false) {
+            log::error!("failed to deactivate profiling: {}", e);
+            anyhow::bail!("failed to deactivate profiling: {}", e);
+        }
+    }
+    Ok(())
+}
+
+/// Dump the profile to the `path`.
+pub fn dump_prof(path: &str) -> anyhow::Result<()> {
+    let mut bytes = CString::new(path)?.into_bytes_with_nul();
+    let ptr = bytes.as_mut_ptr() as *mut c_char;
+    let res = unsafe { tikv_jemalloc_ctl::raw::write(PROF_DUMP, ptr) };
+    match res {
+        Err(e) => {
+            log::error!("failed to dump the profile to {:?}: {}", path, e);
+            Err(anyhow::Error::msg(e.to_string()))
+                .with_context(|| format!("failed to dump the profile to {:?}", path))
+        }
+        Ok(_) => {
+            log::info!("dump profile to {}", path);
+            Ok(())
+        }
+    }
+}
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct JemallocStats {
     pub allocated: u64,
@@ -71,4 +117,37 @@ pub struct JemallocStats {
     pub retained: u64,
     pub dirty: u64,
     pub fragmentation: u64,
+}
+
+// run with `MALLOC_CONF="prof:true" cargo test --ignored`
+#[cfg(test)]
+mod test {
+    const OPT_PROF: &[u8] = b"opt.prof\0";
+
+    fn is_profiling_on() -> bool {
+        match unsafe { tikv_jemalloc_ctl::raw::read(OPT_PROF) } {
+            Err(e) => {
+                // Shouldn't be possible since mem-profiling is set
+                panic!("is_profiling_on: {:?}", e);
+            }
+            Ok(prof) => prof,
+        }
+    }
+
+    #[test]
+    #[ignore]
+    fn test_profile() {
+        use super::*;
+        use std::io::Write;
+        use tempfile::TempDir;
+
+        let dir = TempDir::new().unwrap();
+        let path = dir.path().join("profile.txt");
+
+        activate_prof().unwrap();
+        assert!(is_profiling_on());
+
+        dump_prof(&path.as_path().as_os_str().to_string_lossy()).unwrap();
+        deactivate_prof().unwrap();
+    }
 }
